2f02a21e8f19d0dc47da8df4b050cdaa597295b3
Sensweii <51776743+Sensweii@users.noreply.github.com>
2019-06-19 17:40:16 +0800

    Delete views.py

--- tango_with_django/rango/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Rango says Hey there partner!")
-
-def about(request):
-    return HttpResponse("Rango says this is about page.")
